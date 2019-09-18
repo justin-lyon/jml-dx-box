19365255378ea3ae51c024734c0fd11e45adcd2d
Justin Lyon <jml6487@gmail.com>
2019-09-18 17:12:42 -0500

    add star-rating components

--- force-app/main/default/lwc/star/star.js
+++ force-app/main/default/lwc/star/star.js
@@ -1,8 +1,37 @@
-import { LightningElement } from 'lwc'
+import { LightningElement, api } from 'lwc'
 import Vendors from '@salesforce/resourceUrl/Vendors'
 import { loadStyle } from 'lightning/platformResourceLoader'
 
 export default class Star extends LightningElement {
+  @api star
+
+  @api readOnly = false
+  @api activeColor = '#ffd055'
+  @api inactiveColor = '#d8d8d8'
+
+  clickStar() {
+    if (!this.readOnly) {
+      const selected = new CustomEvent('selected', { detail: this.star.id })
+      this.dispatchEvent(selected)
+    }
+  }
+
+  get classes() {
+    const classes = []
+    classes.push('fa fa-star lwc-star')
+    classes.push(this.isReadOnly())
+    // classes.push(this.color())
+    return classes.join(' ')
+  }
+
+  isReadOnly() {
+    return this.readOnly ? '' : 'clickable'
+  }
+
+  get color() {
+    return 'color:' + (this.star.isActive ? this.activeColor : this.inactiveColor) + ';'
+    // return this.star.isActive ? 'active' : ''
+  }
 
   renderedCallback() {
     loadStyle(this, Vendors + '/fa/css/all.css')

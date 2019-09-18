125f27f4b87d23bff9867d1fc57a813bbf7c93fc
Justin Lyon <jml6487@gmail.com>
2019-09-18 12:13:08 -0500

    add static resource to star.js

--- force-app/main/default/lwc/star/star.js
+++ force-app/main/default/lwc/star/star.js
@@ -1,3 +1,13 @@
 import { LightningElement } from 'lwc'
+import Vendors from '@salesforce/resourceUrl/Vendors'
+import { loadStyle } from 'lightning/platformResourceLoader'
 
-export default class Star extends LightningElement {}
+export default class Star extends LightningElement {
+
+  renderedCallback() {
+    loadStyle(this, Vendors + '/fa/css/all.css')
+      .catch(error => {
+        console.error(error.message)
+      })
+  }
+}

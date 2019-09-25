36962c40b7c17439e84faf82b7ad944542eb591b
Justin Lyon <jml6487@gmail.com>
2019-09-25 11:52:21 -0500

    commit for twitter help

---
+++ force-app/main/default/lwc/lookup/lookup.js
@@ -0,0 +1,43 @@
+import { LightningElement, api, track, wire } from 'lwc'
+import getRecords from '@salesforce/apex/LookupAuraService.getRecords'
+
+export default class Lookup extends LightningElement {
+  @track searchTerm = ''
+  @track records = []
+
+  search () {
+    getRecords({
+      searcher: {
+        searchTerm: this.searchTerm,
+        fieldsByObject: { Account: ['Name', 'Id'] }
+    }})
+      .then(data => {
+        console.log('got data', JSON.parse(JSON.stringify(data)))
+        console.log('got data', JSON.parse(data))
+        this.records = ...data;
+        this.records = data.map(obj => {
+          return ...obj
+        })
+        console.log('records', JSON.stringify(this.records))
+      })
+      .catch(error => {
+        console.error('Error searching records: ', JSON.parse(JSON.stringify(error)))
+      })
+  }
+
+  onChange (event) {
+    event.preventDefault()
+    this.searchTerm = event.target.value
+    console.log('changed', event.target.value)
+
+    if (this.searchTerm.length > 2) {
+      this.search()
+    } else if (this.searchTerm.length == 0) {
+      this.records = []
+    }
+  }
+
+  get hasRecords () {
+    return this.records.length > 0
+  }
+}

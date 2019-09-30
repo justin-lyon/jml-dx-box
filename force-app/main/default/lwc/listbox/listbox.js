15d1799df2d75f4cd6abf306edd8b3fd65709fbd
Justin Lyon <jml6487@gmail.com>
2019-09-30 08:26:22 -0500

    add focus state to input

--- force-app/main/default/lwc/listbox/listbox.js
+++ force-app/main/default/lwc/listbox/listbox.js
@@ -5,8 +5,13 @@ export default class Listbox extends LightningElement {
   @api title
   @api context
   @api iconName
+  @api active = false
 
   get hasRecords () {
     return this.records.length > 0
+  }
+
+  get shown () {
+    return this.records.length > 0 && this.active
   }
 }
--- force-app/main/default/lwc/lookup/lookup.js
+++ force-app/main/default/lwc/lookup/lookup.js
@@ -1,9 +1,11 @@
-import { LightningElement, api, track, wire } from 'lwc'
+import { LightningElement, api, track } from 'lwc'
+import getRecent from '@salesforce/apex/LookupAuraService.getRecent'
 import getRecords from '@salesforce/apex/LookupAuraService.getRecords'
 
 export default class Lookup extends LightningElement {
   @track searchTerm = ''
   @track records = []
+  @track focused = false
 
   @api sobjectName
   @api iconName
@@ -11,21 +13,23 @@ export default class Lookup extends LightningElement {
   @api title = 'Name'
   @api context = 'Id'
 
-  search () {
-    const searcher = {
+  getSearcher () {
+    return {
       searchTerm: this.searchTerm,
-      fieldsByObject: {}
+      objectName: this.sobjectName,
+      fields: [ this.title, this.context ]
     }
-    searcher.fieldsByObject[this.sobjectName] = [ this.title, this.context ]
+  }
+
+  search () {
+    const searcher = this.getSearcher()
 
     console.log('searcher', searcher)
 
     getRecords({searcher})
       .then(data => {
         const newData = JSON.parse(data)
-        this.records = newData.reduce((acc, val) => {
-          return acc.concat(...val)
-        }, [])
+        this.records = newData.flat().sort((a, b) => this.sortAlpha(a, b))
         console.log('records', JSON.parse(JSON.stringify(this.records)))
       })
       .catch(error => {
@@ -38,9 +42,46 @@ export default class Lookup extends LightningElement {
     this.searchTerm = event.target.value
 
     if (this.searchTerm.length > 2) {
-      this.search()
+      this.debounceSearch()
     } else if (this.searchTerm.length === 0) {
       this.records = []
+      this.requestRecent()
     }
+  }
+
+  debounceSearch () {
+    window.clearTimeout(this.delaySearch)
+    this.delaySearch = setTimeout(() => {
+      this.search()
+    }, 300)
+  }
+
+  connectedCallback () {
+    this.requestRecent()
+  }
+
+  requestRecent () {
+    const searcher = this.getSearcher()
+    getRecent({ searcher })
+      .then(data => {
+        this.records = JSON.parse(data)
+      })
+      .catch(error => {
+        console.error('Error requesting recents', JSON.parse(JSON.stringify(error)))
+      })
+  }
+
+  sortAlpha (a, b) {
+    const aName = a[this.title].toLowerCase()
+    const bName = b[this.title].toLowerCase()
+
+    if (aName < bName) return -1
+    if (aName > bName) return 1
+
+    return 0
+  }
+
+  setFocus (event) {
+    this.focused = event.type === 'focus'
   }
 }

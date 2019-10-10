810ed12133daf625e640a0dd2eb87028fdfa5ecc
Justin Lyon <jml6487@gmail.com>
2019-10-10 16:38:30 -0500

    working copy of mdt picker

--- force-app/main/default/lwc/metadataPicker/metadataPicker.js
+++ force-app/main/default/lwc/metadataPicker/metadataPicker.js
@@ -1,3 +1,237 @@
-import { LightningElement } from 'lwc'
+import { LightningElement, api, track } from 'lwc'
+import { ShowToastEvent } from 'lightning/platformShowToastEvent'
+import getFilteredMdt from '@salesforce/apex/MetadataPickerAuraService.getFilteredMdt'
+import getMetadata from '@salesforce/apex/MetadataPickerAuraService.getMetadata'
 
-export default class MetadataPicker extends LightningElement {}
+const ARROW_UP = 'ArrowUp'
+const ARROW_DOWN = 'ArrowDown'
+const ENTER = 'Enter'
+const ESCAPE = 'Escape'
+const ACTIONABLE_KEYS = [ ARROW_UP, ARROW_DOWN, ENTER, ESCAPE ]
+
+export default class Lookup extends LightningElement {
+  @track inputValue = ''
+  @track records = []
+  @track focused = false
+  @track selected = ''
+  @track record
+  @track error
+  @track recordIds
+  @track activeId = ''
+
+  @api mdtName
+  @api iconName
+
+  @api fieldLabel = 'Search'
+  @api placeholder = 'Search...'
+  @api title = 'Name'
+  @api context = 'Id'
+  @api filterBy = ''
+  @api filterTest
+
+  connectedCallback () {
+    this.requestFiltered()
+  }
+
+  @api
+  clear () {
+    this.clearSelection()
+  }
+
+  @api
+  getFilterMetadata (filterTest) {
+    this.filterTest = filterTest
+    this.requestFiltered()
+  }
+
+  get isReadOnly () { return this.record }
+  get showListbox () { return this.focused && this.records.length > 0 && !this.record }
+  get showClear () { return this.record || (!this.record && this.inputValue.length > 0) }
+  get hasError () { return this.error ? this.error.message : '' }
+  get mdtFields () { return [ this.title, this.context ] }
+
+  get containerClasses () {
+    const classes = [ 'slds-combobox_container' ]
+
+    if (this.record) {
+      classes.push('slds-has-selection')
+    }
+
+    return classes.join(' ')
+  }
+
+  get inputClasses () {
+    const classes = [
+      'slds-input',
+      'slds-combobox__input' ]
+
+    if (this.record) {
+      classes.push('slds-combobox__input-value')
+    }
+
+    return classes.join(' ')
+  }
+
+  get comboboxClasses () {
+    const classes = [
+      'slds-combobox',
+      'slds-dropdown-trigger',
+      'slds-dropdown-trigger_click' ]
+
+    if (this.showListbox) {
+      classes.push('slds-is-open')
+    }
+    return classes.join(' ')
+  }
+
+  onKeyup (event) {
+    this.inputValue = event.target.value
+    this.error = null
+
+    const keyAction = {
+      ArrowUp: () => { this.cycleActive(false) },
+      ArrowDown: () => { this.cycleActive(true) },
+      Enter: () => { this.selectItem() },
+      Escape: () => { this.clearSelection() }
+    }
+
+    if (ACTIONABLE_KEYS.includes(event.code)) {
+      keyAction[event.code]()
+
+    } else {
+      if (this.inputValue.length > 2) {
+        this.debounceSearch()
+      } else if (this.inputValue.length === 0) {
+        this.records = []
+        this.requestFiltered()
+      } else {
+        this.error = {
+          message: 'Minimum 2 characters'
+        }
+      }
+    }
+  }
+
+  handleSelected (event) {
+    this.selected = event.detail
+    this.record = this.records.find(record => record.Id === this.selected)
+    this.inputValue = this.record[this.title]
+    this.fireSelected()
+  }
+
+  search () {
+    this.error = null
+
+    getMetadata({
+      searchTerm: this.inputValue,
+      metadataName: this.mdtName,
+      mdtFields: [ this.title, this.context ] })
+      .then(data => {
+        const newData = JSON.parse(data)
+        this.records = newData.flat().sort((a, b) => this.sortAlpha(a, b))
+        this.recordIds = this.getRecordIds()
+
+        if (this.records.length === 0) {
+          this.fireToast({
+            title: 'Info',
+            variant: 'info',
+            message: 'No records found, please refine your search.'
+          })
+        }
+      })
+      .catch(error => {
+        console.error('Error searching records: ', error)
+        this.error = error
+      })
+  }
+
+  debounceSearch () {
+    window.clearTimeout(this.delaySearch)
+    // eslint-disable-next-line @lwc/lwc/no-async-operation
+    this.delaySearch = setTimeout(() => {
+      this.search()
+    }, 300)
+  }
+
+  requestFiltered () {
+    this.error = null
+
+    getFilteredMdt({
+      filterBy: this.filterBy,
+      filterTest: this.filterTest,
+      metadataName: this.mdtName,
+      mdtFields: [ this.title, this.context ] })
+      .then(data => {
+        this.records = JSON.parse(data)
+        this.recordIds = this.getRecordIds()
+      })
+      .catch(error => {
+        console.error('Error requesting recents', error)
+        this.error = error
+      })
+  }
+
+  clearSelection () {
+    this.selected = ''
+    this.record = null
+    this.inputValue = ''
+    this.error = null
+    this.requestFiltered()
+    this.fireSelected()
+  }
+
+  fireSelected () {
+    const selected = new CustomEvent('selected', {
+      detail: { ...this.record }
+    })
+    this.dispatchEvent(selected)
+  }
+
+  cycleActive (forwards) {
+    const currentIndex = this.recordIds.indexOf(this.activeId)
+    if (currentIndex === -1 || currentIndex === this.records.length) {
+      this.activeId = this.recordIds[0]
+    } else if (!forwards && currentIndex === 0) {
+      this.activeId = this.recordIds[this.recordIds.length - 1]
+    } else if (forwards) {
+      this.activeId = this.recordIds[currentIndex + 1]
+    } else {
+      this.activeId = this.recordIds[currentIndex - 1]
+    }
+  }
+
+  selectItem () {
+    if (this.activeId === '' || !this.getRecordIds().includes(this.activeId)) {
+      this.activeId = this.records[0].Id
+      this.record = this.records[0]
+      this.inputValue = this.record[this.title]
+    }
+
+
+    const listbox = this.template.querySelector('c-listbox')
+    listbox.selectItem()
+  }
+
+  setFocus (event) {
+    this.focused = event.type === 'focus'
+  }
+
+  getRecordIds () {
+    return this.records.map(record => record.Id)
+  }
+
+  sortAlpha (a, b) {
+    const aName = a[this.title].toLowerCase()
+    const bName = b[this.title].toLowerCase()
+
+    if (aName < bName) return -1
+    if (aName > bName) return 1
+
+    return 0
+  }
+
+  fireToast (notification) {
+    const toast = new ShowToastEvent(notification)
+    this.dispatchEvent(toast)
+  }
+}
--- force-app/main/default/lwc/wipContainer/wipContainer.js
+++ force-app/main/default/lwc/wipContainer/wipContainer.js
@@ -1,7 +1,31 @@
-/* eslint-disable @lwc/lwc/no-async-operation */
-import { LightningElement, track } from 'lwc'
+import { LightningElement, api, track } from 'lwc'
 import { NavigationMixin } from 'lightning/navigation'
 
 export default class WipContainer extends NavigationMixin(LightningElement) {
+  @track mdtId
+  @track selectedStatus
+  @track statusOptions = [
+    { label: 'User', value: 'user' },
+    { label: 'Account', value: 'account' }
+  ]
+  @track placeholder = 'Select One...'
 
+  @api legend = 'MDT Picker'
+  @api comboLabel = 'Trigger Status'
+  @api pickerLabel = 'Trigger Picker'
+
+  @api filterBy = 'MasterLabel'
+  @api filterTest = 'Account'
+
+  handleStatusSelected (event) {
+    this.selectedStatus = event.target.value
+    const mdtPicker = this.template.querySelector('c-metadata-picker')
+    mdtPicker.clear()
+    mdtPicker.getFilterMetadata(this.selectedStatus)
+  }
+
+  handleMdtSelected (event) {
+    this.mdtId = event.detail ? event.detail.Id : ''
+    this.selectedStatus = event.detail && event.detail[this.filterBy] ? event.detail[this.filterBy].toLowerCase() : this.selectedStatus
+  }
 }

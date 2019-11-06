85d2faeaf4365538914961f7412da6492c521738
Justin Lyon <jml6487@gmail.com>
2019-11-06 13:22:59 -0600

    end of day work in progress

--- force-app/main/default/lwc/accountAlertPanel/accountAlertPanel.js
+++ force-app/main/default/lwc/accountAlertPanel/accountAlertPanel.js
@@ -2,8 +2,56 @@ import { LightningElement, api, track } from 'lwc'
 
 export default class AccountAlertPanel extends LightningElement {
   @api recordId
-  @track alerts
-
-  get hasAlerts () { return this.alerts && this.alerts.length > 0 }
-
+  @track alertsByType = [
+    {
+      name: 'info',
+      label: 'Info',
+      alerts: [
+        {
+          title: 'info 1',
+          message: 'some info',
+          type: {
+            name: 'info',
+            label: 'Info'
+          }
+        }
+      ]
+    },
+    {
+      name: 'warning',
+      label: 'Warning',
+      alerts: [
+        {
+          title: 'warning 1',
+          message: 'some warning',
+          type: {
+            name: 'warning',
+            label: 'Warning'
+          }
+        }
+      ]
+    },
+    {
+      name: 'danger',
+      label: 'Danger',
+      alerts: [
+        {
+          title: 'danger 1',
+          message: 'some danger',
+          type: {
+            name: 'danger',
+            label: 'Danger'
+          }
+        },
+        {
+          title: 'danger 2',
+          message: 'some danger',
+          type: {
+            name: 'danger',
+            label: 'Danger'
+          }
+        }
+      ]
+    }
+  ]
 }
---
+++ force-app/main/default/lwc/alertPanel/alertPanel.js
@@ -0,0 +1,5 @@
+import { LightningElement, api } from 'lwc'
+
+export default class AlertPanel extends LightningElement {
+  @api alertsByType
+}

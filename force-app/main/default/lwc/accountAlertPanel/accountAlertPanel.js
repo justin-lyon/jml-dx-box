341bd0cc2f34373259ddb212fda1810d3670d18e
Justin Lyon <jml6487@gmail.com>
2019-11-07 12:24:08 -0600

    alerts service WIP

--- force-app/main/default/lwc/accountAlertPanel/accountAlertPanel.js
+++ force-app/main/default/lwc/accountAlertPanel/accountAlertPanel.js
@@ -1,57 +1,29 @@
-import { LightningElement, api, track } from 'lwc'
+import { LightningElement, api, track, wire } from 'lwc'
+import getAlertsByAccount from '@salesforce/apex/AccountAlertsPanelAuraService.getAlertsByAccount'
 
 export default class AccountAlertPanel extends LightningElement {
   @api recordId
-  @track alertsByType = [
-    {
-      name: 'info',
-      label: 'Info',
-      alerts: [
-        {
-          title: 'info 1',
-          message: 'some info',
-          type: {
-            name: 'info',
-            label: 'Info'
-          }
-        }
-      ]
-    },
-    {
-      name: 'warning',
-      label: 'Warning',
-      alerts: [
-        {
-          title: 'warning 1',
-          message: 'some warning',
-          type: {
-            name: 'warning',
-            label: 'Warning'
-          }
-        }
-      ]
-    },
-    {
-      name: 'danger',
-      label: 'Danger',
-      alerts: [
-        {
-          title: 'danger 1',
-          message: 'some danger',
-          type: {
-            name: 'danger',
-            label: 'Danger'
-          }
-        },
-        {
-          title: 'danger 2',
-          message: 'some danger',
-          type: {
-            name: 'danger',
-            label: 'Danger'
-          }
-        }
-      ]
+  @track alerts
+
+  @wire(getAlertsByAccount, { accountId: '$recordId' })
+  wiredAlerts ({ error, data }) {
+    console.log('accountId', this.recordId)
+    if (error) {
+      console.error('Error retrieving alerts', error.message)
+      return
     }
-  ]
+    console.log('data', data)
+    this.alerts = data
+  }
+
+  connectedCallback () {
+    console.log('connected callback', this.recordId)
+    getAlertsByAccount({ accountId: this.recordId })
+      .then(data => {
+        console.log('data', data)
+      })
+      .catch(err => {
+        console.error('error getting alerts', err.message)
+      })
+  }
 }

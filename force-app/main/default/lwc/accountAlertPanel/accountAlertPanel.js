7085dc2b93cfa296396be6e97f54df00161d1f4f
Justin Lyon <jml6487@gmail.com>
2019-11-11 09:28:10 -0600

    toast if theres an alert of any kind

--- force-app/main/default/lwc/accountAlertPanel/accountAlertPanel.js
+++ force-app/main/default/lwc/accountAlertPanel/accountAlertPanel.js
@@ -1,4 +1,5 @@
-import { LightningElement, api, track, wire } from 'lwc'
+import { LightningElement, api, track } from 'lwc'
+import { ShowToastEvent } from 'lightning/platformShowToastEvent'
 import getAlertsByAccount from '@salesforce/apex/AccountAlertsPanelAuraService.getAlertsByAccount'
 
 export default class AccountAlertPanel extends LightningElement {
@@ -10,18 +11,17 @@ export default class AccountAlertPanel extends LightningElement {
   @track dangers
 
   connectedCallback () {
-    console.log('connected callback', this.recordId)
     this.fetchAlerts()
   }
 
   fetchAlerts () {
     getAlertsByAccount({ accountId: this.recordId })
       .then(data => {
-        console.log('data', data)
         this.alerts = data
         this.infos = this.filterByType(data, 'Info')
         this.warnings = this.filterByType(data, 'Warning')
         this.dangers = this.filterByType(data, 'Danger')
+        this.toastAlert()
       })
       .catch(err => {
         console.error('error getting alerts', err.message)
@@ -30,5 +30,33 @@ export default class AccountAlertPanel extends LightningElement {
 
   filterByType (data, type) {
     return data.filter(d => d.type === type)
+  }
+
+  toastAlert () {
+    if (this.dangers && this.dangers.length > 0) {
+      this.toast({
+        title: 'Danger',
+        message: 'active alert on this record.',
+        variant: 'error' })
+    } else if (this.warnings && this.warnings.length > 0) {
+      this.toast({
+        title: 'Warning',
+        message: 'active alert on this record.',
+        variant: 'warning' })
+    } else if (this.infos && this.infos.length > 0) {
+      this.toast({
+        title: 'Info',
+        message: 'active alert on this record.',
+        variant: 'info' })
+    }
+  }
+
+  toast ({ title, message, variant }) {
+    const toast = new ShowToastEvent({
+      title,
+      message,
+      variant
+    })
+    this.dispatchEvent(toast)
   }
 }
---
+++ force-app/main/default/lwc/alertPanelList/alertPanelList.js
@@ -0,0 +1,5 @@
+import { LightningElement, api } from 'lwc'
+
+export default class AlertPanelList extends LightningElement {
+  @api alerts
+}

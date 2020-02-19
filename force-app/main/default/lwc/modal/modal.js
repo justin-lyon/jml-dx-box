900a4104d7cd317eb6b9a480763ad1d395dc63da
Justin Lyon <jml6487@gmail.com>
2020-02-19 14:38:12 -0600

    init lwc modal cmp

---
+++ force-app/main/default/lwc/modal/modal.js
@@ -0,0 +1,28 @@
+import { LightningElement, api } from 'lwc'
+
+export default class Modal extends LightningElement {
+  @api isShown = false
+
+  get sectionClass () {
+    return 'slds-modal ' + this.isShown ? ' slds-fade-in-open' : ''
+  }
+
+  get backdropClass () {
+    return 'slds-backdrop ' + this.isShown ? ' slds-backdrop_open' : ''
+  }
+
+  clickedClose () {
+    this.fireClosed()
+  }
+
+  handleKeyup (event) {
+    if (event.keyCode && event.keyCode === 27) {
+      this.fireClosed()
+    }
+  }
+
+  fireClosed () {
+    const close = new CustomEvent('close')
+    this.dispatchEvent(close)
+  }
+}

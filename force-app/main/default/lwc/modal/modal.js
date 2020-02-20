8570ff0431e9cc2dc251d01b4f98fa6346a84aff
Justin Lyon <jml6487@gmail.com>
2020-02-20 06:22:56 -0600

    finalize lwc modal component

--- force-app/main/default/lwc/modal/modal.js
+++ force-app/main/default/lwc/modal/modal.js
@@ -3,12 +3,27 @@ import { LightningElement, api } from 'lwc'
 export default class Modal extends LightningElement {
   @api isShown = false
 
+  @api
+  closeModal () {
+    this.isShown = false
+  }
+
+  @api
+  showModal () {
+    this.isShown = true
+  }
+
+  @api
+  toggleModal () {
+    this.isShown = !this.isShown
+  }
+
   get sectionClass () {
-    return 'slds-modal ' + this.isShown ? ' slds-fade-in-open' : ''
+    return 'slds-modal ' + (this.isShown ? ' slds-fade-in-open' : '')
   }
 
   get backdropClass () {
-    return 'slds-backdrop ' + this.isShown ? ' slds-backdrop_open' : ''
+    return 'slds-backdrop ' + (this.isShown ? ' slds-backdrop_open' : '')
   }
 
   clickedClose () {
